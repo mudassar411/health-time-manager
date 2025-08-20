23faca8858583275c8c2da4468cc0f1f461bdfee
mudassar411 <kmudassar456@gmail.com>
2025-08-20 13:02:56 +0500

    Add files via upload

---
+++ Qwen_bash_20250820_qyz5lqytw.sh
@@ -0,0 +1,9 @@
+# Create a new React app
+npx create-react-app health-time-manager
+cd health-time-manager
+
+# Replace the contents of src/App.js with my code
+# (Copy the entire code I provided)
+
+# Remove unnecessary files
+rm src/App.css src/logo.svg
